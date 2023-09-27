35a631ecb9dca928bf9900931a70f16bfd9d3460
CrioUser <crio-user@criodo.com>
2023-09-27 14:05:09 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,43 +1,47 @@
 import config from "../conf/index.js";
 
 async function init() {
-  //Fetches list of all cities along with their images and description
-  let cities = await fetchCities();
+  try {
+    // Fetches list of all cities along with their images and description
+    let cities = await fetchCities();
 
-  //Updates the DOM with the cities
-
-  cities.forEach((key) => {
-    addCityToDOM(key.id, key.city, key.description, key.image);
-  });
-  
+    // Updates the DOM with the cities
+    cities.forEach((cityData) => {
+      addCityToDOM(cityData.id, cityData.city, cityData.description, cityData.image);
+    });
+  } catch (error) {
+    console.error("Error in init():", error);
+  }
 }
 
-//Implementation of fetch call
+// Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
-  let cities = [{
-    "id": "bengaluru",
-    "city": "Bengaluru",
-    "description": "100+ Places",
-    "image": "https://images.pexels.com/photos/3573382/pexels-photo-3573382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-  }];
+  try {
+    // TODO: Make an actual API call here to fetch cities from your backend
+    // Example using the Fetch API:
+    const response = await fetch(config.apiUrl + "/cities");
+    
+    if (!response.ok) {
+      throw new Error("Failed to fetch cities");
+    }
 
-  return cities;
-
+    const cities = await response.json();
+    return cities;
+  } catch (error) {
+    throw error;
+  }
 }
 
-//Implementation of DOM manipulation to add cities
+// Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-  let container = document.createElement("div")
+  let container = document.createElement("div");
   container.className = "col-6";
-  let innerHTML = `<p>${city}<p><img src="${image}" class="img-response">`
+  let innerHTML = `<p>${city}<p><img src="${image}" class="img-responsive">`;
   container.innerHTML = innerHTML;
 
   document.getElementById("data").appendChild(container);
-
 }
 
 export { init, fetchCities, addCityToDOM };
