5b4062c0f2632c87a5a8b86965df7a6c6c8b85f3
CrioUser <crio-user@criodo.com>
2023-10-14 12:24:28 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,47 +1,70 @@
 import config from "../conf/index.js";
 
 async function init() {
-  try {
-    // Fetches list of all cities along with their images and description
-    let cities = await fetchCities();
+  debugger;
+  //Fetches list of all cities along with their images and description
+  let cities = await fetchCities();
 
-    // Updates the DOM with the cities
-    cities.forEach((cityData) => {
-      addCityToDOM(cityData.id, cityData.city, cityData.description, cityData.image);
+  //Updates the DOM with the cities
+  if (cities) {
+    cities.forEach((key) => {
+      addCityToDOM(key.id, key.city, key.description, key.image);
     });
-  } catch (error) {
-    console.error("Error in init():", error);
   }
 }
 
-// Implementation of fetch call
+//Implementation of fetch call
 async function fetchCities() {
-  try {
-    // TODO: Make an actual API call here to fetch cities from your backend
-    // Example using the Fetch API:
-    const response = await fetch(config.apiUrl + "/cities");
-    
-    if (!response.ok) {
-      throw new Error("Failed to fetch cities");
-    }
-
-    const cities = await response.json();
-    return cities;
-  } catch (error) {
-    throw error;
+  debugger;
+  // TODO: MODULE_CITIES
+  // 1. Fetch cities using the Backend API and return the data
+  try{
+    let res = await fetch(config.backendEndpoint+`/cities`);
+    let data = await res.json();
+    return data;
   }
+  catch(e){
+    return null;
+  }
+  
+
 }
 
-// Implementation of DOM manipulation to add cities
+//Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-  let container = document.createElement("div");
-  container.className = "col-6";
-  let innerHTML = `<p>${city}<p><img src="${image}" class="img-responsive">`;
-  container.innerHTML = innerHTML;
+  const cardRow = document.getElementById("data");
 
-  document.getElementById("data").appendChild(container);
+  const col= document.createElement("div");
+  col.className = "col-12 col-sm-6 col-lg-3 mb-4";
+
+  const anc = document.createElement("a");
+  anc.setAttribute("href",`pages/adventures/?city=${id}`);
+  anc.id = id;
+
+  const titleDiv = document.createElement("div");
+  titleDiv.className = "tile";
+
+  const img = document.createElement("img");
+  img.src = image;
+
+  const titleTextDiv = document.createElement("div");
+  titleTextDiv.className = "tile-text text-center";
+  const h5 = document.createElement("h5");
+  h5.innerText = city;
+  const p = document.createElement("p");
+  p.innerText = description;
+
+  titleTextDiv.append(h5);
+  titleTextDiv.append(p);
+
+  titleDiv.append(img);
+  titleDiv.append(titleTextDiv);
+  anc.append(titleDiv);
+  col.append(anc);
+  // console.log(col);
+  cardRow.append(col);
 }
 
 export { init, fetchCities, addCityToDOM };
